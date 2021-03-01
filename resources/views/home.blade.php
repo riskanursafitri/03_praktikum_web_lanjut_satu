1a10b86bc378e9f51e853bef0184746e9ccbdce5
riskanursafitri <safitririska010@gmail.com>
2021-03-02 01:46:48 +0700

    prak3_1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Selamat Datang!') }}
                 </div>
             </div>
         </div>
